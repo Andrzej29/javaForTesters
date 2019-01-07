c89d6e5310f293981881d3ca3af7fd84986ba52e
Andreas Riemann <anr@mach.de>
2019-01-07 15:26:24 +0100

    erprobte Testbeispiele ergänzt

---
+++ src/test/java/com/javafortesters/chap003myfirsttest/examples/MySecondTest.java
@@ -0,0 +1,10 @@
+package com.javafortesters.chap003myfirsttest.examples;
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+
+public class MySecondTest {
+@Test
+    public void canCheckBoolean () {
+        assertTrue("true", false);
+    }
+}
