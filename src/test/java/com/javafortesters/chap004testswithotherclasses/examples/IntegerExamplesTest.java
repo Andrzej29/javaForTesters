c45b40cbf03174944994828e579146f63d3ef61a
Andreas Riemann <anr@mach.de>
2019-01-08 20:43:16 +0100

    Kapitel 4 (mit anderen Klassen arbeiten) ergänzt

---
+++ src/test/java/com/javafortesters/chap004testswithotherclasses/examples/IntegerExamplesTest.java
@@ -0,0 +1,33 @@
+package com.javafortesters.chap004testswithotherclasses.examples;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class IntegerExamplesTest {
+    @Test
+    public void integerExploration (){
+        Integer four = new Integer(4);
+        assertEquals("intValue returns int 4", 4, four.intValue());
+
+        Integer five = new Integer ("5");
+        assertEquals("intValue returns String", 5, five.intValue());
+
+        Integer six = 6;
+        assertEquals("autoboxing assignment for 6", 6, six.intValue());
+
+    }
+
+    @Test
+    public void canConvertHexToString (){
+        assertEquals("hex 11 is b", "b", Integer.toHexString(11));
+        assertEquals("hex 10 is a", "a", Integer.toHexString(10));
+        assertEquals("hex 3 is 3", "3", Integer.toHexString(3));
+        assertEquals("hex 21 is 15", "15", Integer.toHexString(21));
+    }
+
+    @Test
+    public void canConfirmMinMaxValue () {
+        assertEquals("min value equals -2147483648", -2147483648, Integer.MIN_VALUE);
+        assertEquals("max value equals 2147483647", 2147483647, Integer.MAX_VALUE);
+    }
+}
